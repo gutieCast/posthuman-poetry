6d690db60c09d65f314d742d4ad1a07d7e887a56
A. Gutiérrez Castaño <agutierrez@worlddata.com>
2024-11-26 09:57:26 -0400

    added base vite confiig

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
     sourcemap: true,
     chunkSizeWarningLimit: 1600
   },
-  // base: 'https://gutieCast.github.io/posthuman-poetry'
+  base: 'https://gutieCast.github.io/posthuman-poetry'
 })
